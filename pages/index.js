88618c7a6f4a0b79847b719e83a5b1af71a7ec1f
LaylaKSN <laylasadeghi@googlemail.com>
2023-04-27 23:10:10 +0100

    nightedits

--- pages/index.js
+++ pages/index.js
@@ -18,42 +18,65 @@ export default function Home() {
       <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.description}>
           <h1 className="someHeading">CFG recipes</h1>
+        </div>
+        <div >
           <h2 className="typeOfRecipes">What&apos;s popular</h2>
+          <div className={styles.description}>
+            <div className={styles.recipePic}>
+              <div className={styles.boxForTitle}>
           <h3>
             <Link href="/page1">Creamy vegan mushroom pasta</Link>
           </h3>
-          <h3>
+          </div>
             <Link href="/page1">
               <img src="https://ichef.bbci.co.uk/food/ic/food_16x9_832/recipes/vegan_mushroom_pasta_28266_16x9.jpg" />
             </Link>
-          </h3>
+          </div>
+          <div className={styles.recipePic}>
           <h3>
             <Link href="/page2">
               Roasted squash salad with pomegranate dressing
             </Link>
           </h3>
-          <h3>
             <Link href="/page2">
               {" "}
               <img src="https://realfood.tesco.com/media/images/TESCO-01-Xmas-FestiveMenuHelperRecipes-2910-GM-54718-RoastedSquashSaladWithPomegranteDressingVeganGF-1400x919-1c7e714f-2cc3-4cb7-86e6-7fccd1d28ab5-0-1400x919.jpg" />
             </Link>
-          </h3>
+          </div>
+          <div className={styles.recipePic}>
           <h3>
             <Link href="/page3">Florentine pizza</Link>
           </h3>
           <img src="https://realfood.tesco.com/media/images/Florentine-pizza-LGH-2cc73fa2-bc01-4120-8dbd-c364556b499b-0-1400x919.jpg" />
-          
+          </div>
+          </div>
+          </div>
+          <div>
           <h2 className="typeOfRecipes">Vegan recipes</h2>
+          <div className={styles.description}>
+            <div className={styles.recipePic}>
           <h3>
             <Link href="/page1">Creamy vegan mushroom pasta</Link>{" "}
           </h3>
           <img src="https://ichef.bbci.co.uk/food/ic/food_16x9_832/recipes/vegan_mushroom_pasta_28266_16x9.jpg" />
+          </div>
+          <div className={styles.recipePic}>
           <h3>
             <Link href="/page2">
               Roasted squash salad with pomegranate dressing
             </Link>{" "}
           </h3>
           <img src="https://realfood.tesco.com/media/images/TESCO-01-Xmas-FestiveMenuHelperRecipes-2910-GM-54718-RoastedSquashSaladWithPomegranteDressingVeganGF-1400x919-1c7e714f-2cc3-4cb7-86e6-7fccd1d28ab5-0-1400x919.jpg" />
+          </div>
+          <div className={styles.recipePic}>
+          <h3>
+            <Link href="/page4">
+            Teriyaki mushroom and brown rice bowl
+            </Link>{" "}
+          </h3>
+          <img src="https://realfood.tesco.com/media/images/1400x919-TeriyakiMushroomBrownRiceBowls-68a6a9dd-45cc-403a-8c29-3f2a575df0fe-0-1400x919.jpg" />
+          </div>
+          </div>
         </div>
       </main>
     </>
