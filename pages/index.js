3f2631f3a6494ac5268ea765dec17dba3b328f2c
LaylaKSN <laylasadeghi@googlemail.com>
2023-04-28 09:19:58 +0100

    dessertup

--- pages/index.js
+++ pages/index.js
@@ -18,64 +18,79 @@ export default function Home() {
       <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.description}>
           <h1 className="someHeading">CFG recipes</h1>
+          <form class="search-form" action="#" method="get">
+            <input type="text" placeholder="Ingredient, keyword..." />
+            <button type="submit">Search</button>
+          </form>
         </div>
-        <div >
+        <div>
           <h2 className="typeOfRecipes">What&apos;s popular</h2>
           <div className={styles.description}>
             <div className={styles.recipePic}>
               <div className={styles.boxForTitle}>
-          <h3>
-            <Link href="/page1">Creamy vegan mushroom pasta</Link>
-          </h3>
+                <h3>
+                  <Link href="/page1">Creamy vegan mushroom pasta</Link>
+                </h3>
+              </div>
+              <Link href="/page1">
+                <img src="https://ichef.bbci.co.uk/food/ic/food_16x9_832/recipes/vegan_mushroom_pasta_28266_16x9.jpg" />
+              </Link>
+            </div>
+            <div className={styles.recipePic}>
+              <h3>
+                <Link href="/page2">
+                  Roasted squash salad with pomegranate dressing
+                </Link>
+              </h3>
+              <Link href="/page2">
+                {" "}
+                <img src="https://realfood.tesco.com/media/images/TESCO-01-Xmas-FestiveMenuHelperRecipes-2910-GM-54718-RoastedSquashSaladWithPomegranteDressingVeganGF-1400x919-1c7e714f-2cc3-4cb7-86e6-7fccd1d28ab5-0-1400x919.jpg" />
+              </Link>
+            </div>
+            <div className={styles.recipePic}>
+              <h3>
+                <Link href="/page3">Florentine pizza</Link>
+              </h3>
+              <img src="https://realfood.tesco.com/media/images/Florentine-pizza-LGH-2cc73fa2-bc01-4120-8dbd-c364556b499b-0-1400x919.jpg" />
+            </div>
           </div>
-            <Link href="/page1">
-              <img src="https://ichef.bbci.co.uk/food/ic/food_16x9_832/recipes/vegan_mushroom_pasta_28266_16x9.jpg" />
-            </Link>
-          </div>
-          <div className={styles.recipePic}>
-          <h3>
-            <Link href="/page2">
-              Roasted squash salad with pomegranate dressing
-            </Link>
-          </h3>
-            <Link href="/page2">
-              {" "}
-              <img src="https://realfood.tesco.com/media/images/TESCO-01-Xmas-FestiveMenuHelperRecipes-2910-GM-54718-RoastedSquashSaladWithPomegranteDressingVeganGF-1400x919-1c7e714f-2cc3-4cb7-86e6-7fccd1d28ab5-0-1400x919.jpg" />
-            </Link>
-          </div>
-          <div className={styles.recipePic}>
-          <h3>
-            <Link href="/page3">Florentine pizza</Link>
-          </h3>
-          <img src="https://realfood.tesco.com/media/images/Florentine-pizza-LGH-2cc73fa2-bc01-4120-8dbd-c364556b499b-0-1400x919.jpg" />
-          </div>
-          </div>
-          </div>
-          <div>
+        </div>
+        <div>
           <h2 className="typeOfRecipes">Vegan recipes</h2>
           <div className={styles.description}>
             <div className={styles.recipePic}>
-          <h3>
-            <Link href="/page1">Creamy vegan mushroom pasta</Link>{" "}
-          </h3>
-          <img src="https://ichef.bbci.co.uk/food/ic/food_16x9_832/recipes/vegan_mushroom_pasta_28266_16x9.jpg" />
+              <h3>
+                <Link href="/page1">Creamy vegan mushroom pasta</Link>{" "}
+              </h3>
+              <img src="https://ichef.bbci.co.uk/food/ic/food_16x9_832/recipes/vegan_mushroom_pasta_28266_16x9.jpg" />
+            </div>
+            <div className={styles.recipePic}>
+              <h3>
+                <Link href="/page2">
+                  Roasted squash salad with pomegranate dressing
+                </Link>{" "}
+              </h3>
+              <img src="https://realfood.tesco.com/media/images/TESCO-01-Xmas-FestiveMenuHelperRecipes-2910-GM-54718-RoastedSquashSaladWithPomegranteDressingVeganGF-1400x919-1c7e714f-2cc3-4cb7-86e6-7fccd1d28ab5-0-1400x919.jpg" />
+            </div>
+            <div className={styles.recipePic}>
+              <h3>
+                <Link href="/page4">Teriyaki mushroom and brown rice bowl</Link>{" "}
+              </h3>
+              <img src="https://realfood.tesco.com/media/images/1400x919-TeriyakiMushroomBrownRiceBowls-68a6a9dd-45cc-403a-8c29-3f2a575df0fe-0-1400x919.jpg" />
+            </div>
           </div>
-          <div className={styles.recipePic}>
-          <h3>
-            <Link href="/page2">
-              Roasted squash salad with pomegranate dressing
-            </Link>{" "}
-          </h3>
-          <img src="https://realfood.tesco.com/media/images/TESCO-01-Xmas-FestiveMenuHelperRecipes-2910-GM-54718-RoastedSquashSaladWithPomegranteDressingVeganGF-1400x919-1c7e714f-2cc3-4cb7-86e6-7fccd1d28ab5-0-1400x919.jpg" />
-          </div>
-          <div className={styles.recipePic}>
-          <h3>
-            <Link href="/page4">
-            Teriyaki mushroom and brown rice bowl
-            </Link>{" "}
-          </h3>
-          <img src="https://realfood.tesco.com/media/images/1400x919-TeriyakiMushroomBrownRiceBowls-68a6a9dd-45cc-403a-8c29-3f2a575df0fe-0-1400x919.jpg" />
-          </div>
+        </div>
+        <div>
+          <h2 className="typeOfRecipes">Desserts</h2>
+          <div className={styles.description}>
+            <div className={styles.recipePic}>
+              <button class="button">
+                <a href="/page4">
+                  Blackberry filo tart
+                  <img src="https://realfood.tesco.com/media/images/RFO-1400x919-Blackberry-filo-tart-8b86384a-c2ac-4937-b9b3-9c21167bee52-0-1400x919.jpg" />
+                </a>
+              </button>
+            </div>
           </div>
         </div>
       </main>
