5e85cb9044990d3cbd157c6d4d87b358ae2783d5
LaylaKSN <laylasadeghi@googlemail.com>
2023-05-01 20:00:18 +0100

    chan

--- pages/index.js
+++ pages/index.js
@@ -33,14 +33,13 @@ export default function Home() {
         <div>
           <h2 className="typeOfRecipes">What&apos;s popular</h2>
           <div className={styles.description}>
-            <div className={styles.recipePic}>
-              <div className={styles.boxForTitle}>
+              <div className={styles.recipePic}>
                 <h3>
                   <Link href="/page1">Creamy vegan mushroom pasta</Link>
                 </h3>
-              </div>
               <Link href="/page1">
-                <img src="https://ichef.bbci.co.uk/food/ic/food_16x9_832/recipes/vegan_mushroom_pasta_28266_16x9.jpg" />
+                {" "}
+                <img className="recipeImage" src="https://ichef.bbci.co.uk/food/ic/food_16x9_832/recipes/vegan_mushroom_pasta_28266_16x9.jpg" />
               </Link>
             </div>
             <div className={styles.recipePic}>
@@ -51,7 +50,7 @@ export default function Home() {
               </h3>
               <Link href="/page2">
                 {" "}
-                <img src="https://realfood.tesco.com/media/images/TESCO-01-Xmas-FestiveMenuHelperRecipes-2910-GM-54718-RoastedSquashSaladWithPomegranteDressingVeganGF-1400x919-1c7e714f-2cc3-4cb7-86e6-7fccd1d28ab5-0-1400x919.jpg" />
+                <img className="recipeImage"  src="https://realfood.tesco.com/media/images/TESCO-01-Xmas-FestiveMenuHelperRecipes-2910-GM-54718-RoastedSquashSaladWithPomegranteDressingVeganGF-1400x919-1c7e714f-2cc3-4cb7-86e6-7fccd1d28ab5-0-1400x919.jpg" />
               </Link>
             </div>
             <div className={styles.recipePic}>
@@ -60,7 +59,7 @@ export default function Home() {
               </h3>
               <Link href="/page3">
                 {" "}
-              <img src="https://realfood.tesco.com/media/images/Florentine-pizza-LGH-2cc73fa2-bc01-4120-8dbd-c364556b499b-0-1400x919.jpg" />
+              <img className="recipeImage"  src="https://realfood.tesco.com/media/images/Florentine-pizza-LGH-2cc73fa2-bc01-4120-8dbd-c364556b499b-0-1400x919.jpg" />
             </Link>
             </div>
           </div>
